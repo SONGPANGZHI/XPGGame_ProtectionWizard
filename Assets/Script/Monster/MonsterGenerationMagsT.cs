8216bda7fa68cc9ad6e6a8d1d52b8d6e5cf70ae1
Venus_Rosemary <2371495710@qq.com>
2025-03-10 13:38:01 +0800

    第二关怪物生成修改

--- Assets/Script/Monster/MonsterGenerationMagsT.cs
+++ Assets/Script/Monster/MonsterGenerationMagsT.cs
@@ -18,15 +18,6 @@ public class MonsterGenerationMagsT : MonoBehaviour
     [SerializeField]
     private GameObject evilPrefab3;
 
-    [SerializeField]
-    private int randomMin = 5;
-
-    [SerializeField]
-    private int randomMax = 8;
-
-    [SerializeField]
-    private int goodnessMonsterNum = 2;
-
     public int score = 0; // 分数
 
     private void OnEnable()
@@ -42,134 +33,26 @@ public class MonsterGenerationMagsT : MonoBehaviour
     public void FristCreateMonter()
     {
         monsterPointList.ShuffleT();
-
-        int goodID = RandomMonterType();
-        if (goodID == 0)
-        {
-            //本轮多个善良怪
-            SpawnMultipleMonsters();
-        }
-        else
-            SpawnOneMonsters();
-
+        Set_MonsterSeneration();
     }
 
-    //2个npc的情况
-    //生成多个善良怪
-    public void SpawnMultipleMonsters()
-    {
-        score = ScoreManagement.Instance.TotalScore;
-        if (score>=0 && score < 100)//只生成npc和一种怪
-        {
-            for (int i = 0; i < RandomMonterNum(); i++)
-            {
-                if (i < goodnessMonsterNum)
-                {
-                    GameObject ga = Instantiate(goodnessPrefab2, monsterPointList[i].transform);
-                }
-                else
-                {
-                    GameObject ga = Instantiate(evilPrefab1, monsterPointList[i].transform);
-                }
-            }
-            if (!firstBool)
-            {
-                firstBool = true;
-                ShowMonsterDescPlane(0);
-            }
-        }
-        //else if(score >= 100 && score < 200)
-        //{
-        //    for (int i = 0; i < RandomMonterNum(); i++)
-        //    {
-        //        if (i < goodnessMonsterNum)
-        //        {
-        //            GameObject ga = Instantiate(goodnessPrefab2, monsterPointList[i].transform);
-        //        }
-        //        else
-        //        {
-        //            GameObject ga = Instantiate(evilPrefab2, monsterPointList[i].transform);
-        //        }
-        //    }
-        //}
-        else if (score >= 100 && score < 200)
-        {
-            int num1 = RandomMonterNum();
-            int a = num1 - goodnessMonsterNum;
-            int cubeCount = Random.Range(1, a);
-            for (int i = 0; i < num1; i++)
-            {
-                if (i < goodnessMonsterNum)//2
-                {
-                    GameObject ga = Instantiate(goodnessPrefab2, monsterPointList[i].transform);
-                }
-                else if(i>= goodnessMonsterNum && i< goodnessMonsterNum+ cubeCount)//2---(2+x)
-                {
-                    GameObject ga = Instantiate(evilPrefab1, monsterPointList[i].transform);
-                }
-                else if (i >= goodnessMonsterNum+ cubeCount && i < num1)//(2-x)---总数
-                {
-                    GameObject ga = Instantiate(evilPrefab2, monsterPointList[i].transform);
-                }
-            }
-            if (!secondBool)
-            {
-                secondBool = true;
-                ShowMonsterDescPlane(1);
-            }
-        }
-        else if (score >= 200)
-        {
-            int num1 = RandomMonterNum();
-            int num = num1 - goodnessMonsterNum;//6-2  4
-            int a = Random.Range(1, num - 1);//（1,3）                1和2之间随机 2
-            int circleCount = num - a;//4-2                 随到2     得到2
-            int b = Random.Range(1, circleCount);//1---2    1
-            int c = num - a - b;//                          1
-            //0/1------2/3------4------5
-            for (int i = 0; i < num1; i++)
-            {
-                if (i < goodnessMonsterNum)//2
-                {
-                    GameObject ga = Instantiate(goodnessPrefab2, monsterPointList[i].transform);
-                }
-                else if (i >= goodnessMonsterNum && i < goodnessMonsterNum + a)
-                {
-                    GameObject ga = Instantiate(evilPrefab1, monsterPointList[i].transform);
-                }
-                else if (i >= goodnessMonsterNum + a && i < goodnessMonsterNum + a+ b)
-                {
-                    GameObject ga = Instantiate(evilPrefab2, monsterPointList[i].transform);
-                }
-                else if (i >= goodnessMonsterNum + a + b && i < num1)
-                {
-                    GameObject ga = Instantiate(evilPrefab3, monsterPointList[i].transform);
-                }
-            }
-            if (!thirdBool)
-            {
-                thirdBool = true;
-                ShowMonsterDescPlane(2);
-            }
-        }
-
-    }
-
-    //1个npc的情况
+    [SerializeField]
+    private int scorePhaseOne = 100;
+    [SerializeField]
+    private int scorePhaseTwo = 200;
+    [SerializeField]
+    private int scorePhaseTree = 300;
 
     bool firstBool;
     bool secondBool;
     bool thirdBool;
-
-
-
-    //生成最少一个
-    public void SpawnOneMonsters()
+    public void Set_MonsterSeneration()
     {
         score = ScoreManagement.Instance.TotalScore;
-        if (score >= 0 && score < 100)
+        if (score >= 0 && score < scorePhaseOne)
         {
-            for (int i = 0; i < RandomMonterNum(); i++)
+            int num = Random.Range(3,5);
+            for (int i = 0; i < num; i++)
             {
                 if (i == 0)
                 {
@@ -180,119 +63,70 @@ public class MonsterGenerationMagsT : MonoBehaviour
                     GameObject ga = Instantiate(evilPrefab1, monsterPointList[i].transform);
                 }
             }
-
             if (!firstBool)
             {
                 firstBool = true;
                 ShowMonsterDescPlane(0);
             }
-                
+
         }
-        //else if(score >= 100 && score < 200)
-        //{
-        //    for (int i = 0; i < RandomMonterNum(); i++)
-        //    {
-        //        if (i == 0)
-        //        {
-        //            GameObject ga = Instantiate(goodnessPrefab2, monsterPointList[i].transform);
-        //        }
-        //        else
-        //        {
-        //            GameObject ga = Instantiate(evilPrefab2, monsterPointList[i].transform);
-        //        }
-        //    }
-        //}
-        else if (score >= 100 && score < 200)
+        else if (score >= scorePhaseOne && score < scorePhaseTwo)
         {
-            int num1 = RandomMonterNum();//                5   5-1=4
-            int a = num1 - 1;
-            int cubeCount = Random.Range(1, a);//1到4之间   3
-            //0------1/2/3------4/5
-            for (int i = 0; i < num1; i++)
+            int a = Random.Range(3, 5);
+            int b = Random.Range(1, 3);
+            int num = a+b+1;
+            for (int i = 0; i < num; i++)
             {
                 if (i == 0)
                 {
                     GameObject ga = Instantiate(goodnessPrefab2, monsterPointList[i].transform);
                 }
-                else if(i>0&&i<= cubeCount)
+                else if(i > 0 && i <= a)
                 {
                     GameObject ga = Instantiate(evilPrefab1, monsterPointList[i].transform);
                 }
-                else if (i > cubeCount && i < num1)
+                else if (i >= a && i<num)
                 {
                     GameObject ga = Instantiate(evilPrefab2, monsterPointList[i].transform);
                 }
             }
-
             if (!secondBool)
             {
                 secondBool = true;
                 ShowMonsterDescPlane(1);
             }
-        }
-        else if (score >= 200)
-        {
-            int num1 = RandomMonterNum();//5-1           4
-            int num = num1 - 1;
-            int a = Random.Range(1, num - 1);//1---3        1和2之间随机 2
-            int circleCount = num - a;//4-2                 2
-            int b = Random.Range(1, circleCount);//1---2    1
-            int c = num - a - b;//                          1
 
-            //0------(1/2/3)------(4)------(5)
-            for (int i = 0; i < num1; i++)
+        }
+        else if (score >= scorePhaseTwo)
+        {
+            int a = Random.Range(3, 5);
+            int b = Random.Range(1, 3);
+            int num = a + b + 4;
+            for (int i = 0; i < num; i++)
             {
-                if (i == 0)
+                if (i >= 0 && i < 2)
                 {
                     GameObject ga = Instantiate(goodnessPrefab2, monsterPointList[i].transform);
                 }
-                else if (i > 0 && i <= a)
+                else if (i >= 2 && i < a + 2)
                 {
                     GameObject ga = Instantiate(evilPrefab1, monsterPointList[i].transform);
                 }
-                else if (i > a && i <= a+b)
+                else if (i >= a + 2 && i < a + 2 + b)
                 {
                     GameObject ga = Instantiate(evilPrefab2, monsterPointList[i].transform);
                 }
-                else if (i > a + b && i<num1)
+                else if (i >= a + 2 + b)
                 {
                     GameObject ga = Instantiate(evilPrefab3, monsterPointList[i].transform);
                 }
             }
-
             if (!thirdBool)
             {
                 thirdBool = true;
                 ShowMonsterDescPlane(2);
             }
         }
-
-
-    }
-
-    //public int randomMN;
-    //第一次随机生成怪物数量
-    public int RandomMonterNum()
-    {
-        int monterNum = Random.Range(randomMin, randomMax);
-        return monterNum;
-    }
-
-    //随机善良、邪恶怪物  0为善良
-    public int RandomMonterType()
-    {
-        int monterType = Random.Range(0, 2);
-        if (monterType == 0)
-        {
-            DoubleHitManager.Instance.DebugColorRed("本轮有多个善良的小怪");
-            return 0;
-        }
-        else
-        {
-            DoubleHitManager.Instance.DebugColorRed("本轮至少有一个善良的小怪");
-            return 1;
-        }
-
     }
 
     //显示怪物提醒界面
